f494e579772711970b19205652699ac8e97e41fa
Alan Chavarin <alanchavarin4@hotmail.com>
2023-09-13 00:54:06 -0700

    fixed iframe title

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,7 +3,7 @@ import ResumeCSS from './styles/Resume.module.css'
 
 function Resume() {
   return (
-    <iframe src="pdf/Resume-AlanChavarin.pdf" frameborder="0" className={ResumeCSS.iframe}></iframe>
+    <iframe src="pdf/Resume-AlanChavarin.pdf" frameborder="0" className={ResumeCSS.iframe} title="Alan Chavarin's Resume"></iframe>
   )
 }
 export default Resume
