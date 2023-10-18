fc5d576633a291ffadebd8930487eae488c97118
Alan Chavarin <alanchavarin4@hotmail.com>
2023-10-18 08:58:34 -0700

    docx resume thats ats friendly

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,7 +3,7 @@ import ResumeCSS from './styles/Resume.module.css'
 
 function Resume() {
   return (
-    <iframe src="pdf/Resume-AlanChavarin.pdf" frameborder="0" className={ResumeCSS.iframe} title="Alan Chavarin's Resume"></iframe>
+    <iframe src="pdf/AlanChavarin_Resume.docx" frameborder="0" className={ResumeCSS.iframe} title="Alan Chavarin's Resume"></iframe>
   )
 }
 export default Resume
